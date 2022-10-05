d5e784a1704c155f184be0a686cbe949c53c6c20
Marce Concepción <marcelinocb@gmail.com>
2022-10-05 22:35:52 +0100

    soluciones creadas

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-import { fizzbuzzRangeGenerator } from "./lib/fizzbuzz";
+import { fizzbuzzRange } from "./lib/fizzbuzz";
 
 /**
- * Colocar el FizzBuzz en su sitio
+ * Colocar el FizzBuzz en su sitio.
+ * No es necesario tocar nada de esta función.
  */
 function setFizzbuzz() {
   const fizzbuzzList = document.querySelector('#fizzbuzz ul');
-  fizzbuzzList.innerHTML = fizzbuzzRangeGenerator(1,100).map( x => `<li>${x}</li>`).join('');
+  fizzbuzzList.innerHTML = fizzbuzzRange(1,100).map( x => `<li>${x}</li>`).join('');
 }
 
 setFizzbuzz()
