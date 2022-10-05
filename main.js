75b445b6d992f6edbd1a001c211f3dea2db6b074
Marce Concepción <marcelinocb@gmail.com>
2022-10-05 22:44:11 +0100

    formulario morse

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { fizzbuzzRange} from "./lib/fizzbuzz";
+import { text2morse } from "./lib/morse";
 
 /**
  * Colocar el FizzBuzz en su sitio
@@ -8,4 +9,16 @@ function setFizzbuzz() {
   fizzbuzzList.innerHTML = fizzbuzzRange(1,100).map( x => `<li>${x}</li>`).join('');
 }
 
-setFizzbuzz()
+setFizzbuzz();
+
+document.getElementById('text2morse').addEventListener('click', () => {
+  const text = document.getElementById('text').value;
+  const morse = text2morse(text);
+  document.getElementById('morse').value = morse;
+})
+
+document.getElementById('morse2text').addEventListener('click', () => {
+  const morse = document.getElementById('morse').value;
+  const text = morse2text(morse);
+  document.getElementById('text').value = morse;
+})
